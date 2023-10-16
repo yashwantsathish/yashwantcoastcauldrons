d5912fcdda2c722f57a10cf4617c838c9c4af6eb
yashwantsathish <46503623+yashwantsathish@users.noreply.github.com>
2023-10-15 23:28:04 -0700

    catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,28 +19,42 @@ def get_catalog():
     with db.engine.begin() as connection:
         num_green = connection.execute(sqlalchemy.text("SELECT quantity FROM potions " \
                                                                "WHERE sku = 'green'"))
+        num_yellow = connection.execute(sqlalchemy.text("SELECT quantity FROM potions " \
+                                                               "WHERE sku = 'yellow'"))
         num_green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
         
     num_green = num_green.first()[0]
+    num_yellow = num_yellow.first()[0]
     num_green_ml = num_green_ml.first()[0]
 
     print("Catalog - green: " + str(num_green) + " gml: " + str(num_green_ml))
     
     # Can return a max of 6 unique skus.
-    if num_green == 0:
+    if num_green + num_yellow == 0:
         return []
 
-    if(num_green > 0):
+    if num_green > 0:
         ret_list.append(
                 {
                     "sku": "green",
                     "name": "green",
                     "quantity": num_green,
-                    "price":  50,
+                    "price": 50,
                     "potion_type": [0, 100, 0, 0],
                 }
         )
-    
+    if num_yellow > 0:
+        ret_list.append(
+                {
+                    "sku": "yellow",
+                    "name": "yellow",
+                    "quantity": num_yellow,
+                    "price": 50,
+                    "potion_type": [50, 50, 0, 0],
+                }
+        )
+
+    print(ret_list)
     return ret_list
                 
             

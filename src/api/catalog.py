213f49ff3a2cd59aea963dd718a6c16e7138614a
yashwantsathish <46503623+yashwantsathish@users.noreply.github.com>
2023-10-17 18:57:59 -0700

    commit

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -39,7 +39,7 @@ def get_catalog():
                     "sku": "green",
                     "name": "green",
                     "quantity": num_green,
-                    "price": 50,
+                    "price": 40,
                     "potion_type": [0, 100, 0, 0],
                 }
         )

b93cd93b7b166326e991195815c4c7cdcbe92b6c
yashwantsathish <46503623+yashwantsathish@users.noreply.github.com>
2023-10-28 20:44:10 -0700

    commit

--- src/api/carts.py
+++ src/api/carts.py
@@ -63,6 +63,7 @@ def search_orders(
 
     # Set Current Index
     curr_index = int(search_page) if search_page else 1
+    print(curr_index)
     
     # Find what to sort by
     if sort_col == search_sort_options.customer_name:
